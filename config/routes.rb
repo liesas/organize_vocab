9d349df99a1c37f6d8199a4bc86a2292dfa4d710
liesa <lschoenburg@gmx.de>
2023-03-06 13:11:26 +0100

    Implement User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+
+  namespace :api do
+    namespace :v1 do
+      resources :users
+    end
+  end
 end
