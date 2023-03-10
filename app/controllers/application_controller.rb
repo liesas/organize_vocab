1887a88717a4b35de73718b35730d7d09eaba3ab
liesa <lschoenburg@gmx.de>
2023-03-10 19:50:05 +0100

    Improve handling API errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
+
+  private
+    def not_found
+      render status: :not_found
+    end
 end
